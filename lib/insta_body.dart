b6279c3b812d489a68ce448190b08b2a3d8ea0b9
copeninga <45181745+copeninga@users.noreply.github.com>
2018-12-17 14:22:37 -0600

    Update insta_body.dart

--- lib/insta_body.dart
+++ lib/insta_body.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
-import 'pacakge:insta/insta_list.dart
+import 'pacakge:insta/insta_list.dart';
 
 
 
-class Instabody extends Stateless
+class Instabody extends StatelessWidget {
+  @override
+  Widget build(BuildingContext context) {
+    return new Column(
+      children: <Widget>[
+        Flexible(child: InstaList(),)
+        ], // <Widget>[]
+        ); // Column 
+        }
+
+        }
+
+        
+        
